30c31655717054ca566543a0fa470b515db2e2d7
Saruizz <saruizp.dev@gmail.com>
2026-05-26 11:31:25 -0500

    test(store): add unit tests for auth.store slice actions and state transitions

--- tests/setup/setupTests.js
+++ tests/setup/setupTests.js
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom/vitest';
+
+const store = {};
+const localStorageMock = {
+  getItem: (key) => store[key] ?? null,
+  setItem: (key, value) => { store[key] = String(value); },
+  removeItem: (key) => { delete store[key]; },
+  clear: () => { Object.keys(store).forEach((k) => delete store[k]); },
+  get length() { return Object.keys(store).length; },
+  key: (index) => Object.keys(store)[index] ?? null,
+};
+
+Object.defineProperty(globalThis, 'localStorage', {
+  value: localStorageMock,
+  writable: true,
+  configurable: true,
+});
